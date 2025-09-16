ec1d2172c97dc2e6a60fff6d6889484098aae91c
flurrybun <parkercasey007@gmail.com>
2025-09-16 14:55:12 -0400

    simplify tier location settings, add stars/moons label option

--- src/modified/LevelInfoLayer.cpp
+++ src/modified/LevelInfoLayer.cpp
@@ -8,8 +8,6 @@
 #include "RatingsManager.h"
 #include "Utils.h"
 #include "layers/GDDLLevelInfoPopup.h"
-#include "settings/ButtonPositionSettingV3.h"
-#include "settings/UseOldTierLabelSettingV3.h"
 #include "settings/ExcludeRangeSettingV3.h"
 #include "layers/GDDLAdvancedLevelInfoPopup.h"
 
@@ -19,6 +17,7 @@ class $modify(GDDLInfoLayer, LevelInfoLayer) {
     struct Fields {
         EventListener<web::WebTask> infoLayerGetRatingListener;
         bool gddlTierUpdated = false;
+        CCMenuItemSpriteExtra* tierButton = nullptr;
         GDDLAdvancedLevelInfoPopup* advancedLevelInfoPopup = nullptr;
     };
 
@@ -49,78 +48,63 @@ class $modify(GDDLInfoLayer, LevelInfoLayer) {
             }
         });
 
-        const bool isDemon = m_level->m_stars == 10;
-        if (isDemon && Utils::notExcluded(m_level->m_levelID)) {
-            m_fields->gddlTierUpdated = false;
-            const bool displayAsLabel = static_pointer_cast<UseOldTierLabelSettingV3>(Mod::get()->getSetting("use-old-tier-label"))->isEnabled();
-            if (!displayAsLabel) {
-                const auto buttonPositionSetting = static_pointer_cast<ButtonPositionSettingV3>(Mod::get()->getSetting("button-position"))->getPosition();
-                CCPoint menuPosition, buttonPosition;
-                CCSize menuSize;
-                float buttonScale = 1.0f;
-                if (buttonPositionSetting != DEFAULT) {
-                    const auto levelNameLabel = typeinfo_cast<CCLabelBMFont *>(getChildByID("title-label"));
-                    const auto levelNamePosition = levelNameLabel->getPosition();
-                    const auto levelNameSize = levelNameLabel->getScaledContentSize();
-                    if (buttonPositionSetting == TO_THE_RIGHT_OF_THE_LEVEL_TITLE) { // right
-                        menuPosition = CCPoint{levelNamePosition.x + levelNameSize.width / 2.0f,
-                                               levelNamePosition.y - 14.5f};
-                        // move the weekly/event label
-                        if (m_level->m_dailyID > 100000) { // weekly: 100001 - 200000, event: 200000+, thanks prevter <3
-                            const auto weeklyLabelNode = getChildByID("daily-label");
-                            if (weeklyLabelNode != nullptr) {
-                                const auto weeklyLabel = typeinfo_cast<CCLabelBMFont*>(weeklyLabelNode);
-                                const auto weeklyLabelX = weeklyLabel->getPosition().x;
-                                weeklyLabel->setPosition({weeklyLabelX + 23.0f, weeklyLabel->getPosition().y});
-                            }
-                        }
-                    } else { // left
-                        menuPosition = CCPoint{levelNamePosition.x - levelNameSize.width / 2.0f - 25.0f,
-                                               levelNamePosition.y - 14.0f};
-                    }
-                    menuSize = CCSize{25, 25};
-                    buttonPosition = CCPoint{12.5f, 12.5f};
-                    buttonScale = 0.5f;
-                } else {
-                    const auto diffPosition = m_difficultySprite->getPosition();
-                    const auto diffSize = m_difficultySprite->getContentSize();
-                    menuPosition =
-                            CCPoint{diffPosition.x - 50 - diffSize.width / 2, diffPosition.y - diffSize.height / 3.2f};
-                    menuSize = CCSize{50, 50};
-                    buttonPosition = CCPoint{25, 25};
+        if (m_level->m_stars != 10 || !Utils::notExcluded(m_level->m_levelID)) return true;
+
+        m_fields->gddlTierUpdated = false;
+
+        const auto menu = CCMenu::create();
+        menu->setContentSize({35.2f, 35.2f});
+        menu->setID("rating-menu"_spr);
+        addChild(menu);
+
+        const std::string buttonPosition = Mod::get()->getSettingValue<std::string>("button-position");
+
+        if (buttonPosition == "Left of Difficulty") {
+            menu->setPosition(m_difficultySprite->getPosition() + ccp(-45.0f, 6.0f));
+        } else if (buttonPosition == "Stars/Moons Label") {
+            m_starsLabel->setOpacity(0);
+
+            menu->setPosition(m_starsLabel->getPosition());
+            menu->setAnchorPoint(m_starsLabel->getAnchorPoint());
+            menu->setScale(0.4f);
+        } else {
+            const auto titleLabel = getChildByID("title-label");
+
+            const bool isLeft = buttonPosition == "Left of Title";
+            const auto offset = ccp(
+                (isLeft ? -1 : 1) * (titleLabel->getScaledContentWidth() / 2.0f + 10.0f),
+                -1.5f
+            );
+
+            menu->setPosition(titleLabel->getPosition() + offset);
+
+            // move the weekly/event label
+            // weekly: 100001 - 200000, event: 200000+, thanks prevter <3
+            if (!isLeft && m_level->m_dailyID > 100000) {
+                if (const auto weeklyLabel = getChildByID("daily-label")) {
+                    weeklyLabel->setPositionX(weeklyLabel->getPositionX() + 23.0f);
                 }
-                placeGDDLButton(menuPosition, menuSize, buttonPosition, buttonScale);
-            } else {
-                const auto tierLabelSprite = CCLabelBMFont::create("Tier ??", "bigFont.fnt");
-                tierLabelSprite->setID("gddl-rating_label"_spr);
-                float labelShiftRows = 1.0f;
-                if (m_level->m_coins > 0) {
-                    labelShiftRows += 1.0f;
-                }
-                const auto moveRowsSetting = static_pointer_cast<UseOldTierLabelSettingV3>(Mod::get()->getSetting("use-old-tier-label"))->getPositionOffset();
-                if (moveRowsSetting == -1) {
-                    labelShiftRows = -4.5f;
-                } else {
-                    labelShiftRows += static_cast<float>(moveRowsSetting);
-                }
-                const auto tierLabel =
-                        CCMenuItemSpriteExtra::create(tierLabelSprite, this, menu_selector(GDDLInfoLayer::onGDDLInfo));
-                tierLabelSprite->setScale(0.4f);
-                const auto starsLabel = m_starsLabel;
-                const auto tierLabelMenu = CCMenu::create();
-                tierLabelMenu->setPosition(
-                        {starsLabel->getPositionX(), starsLabel->getPositionY() - labelShiftRows * 15.5f});
-                tierLabelMenu->addChild(tierLabel, 10);
-                addChild(tierLabelMenu);
-                reorderChild(tierLabelMenu, 10);
             }
 
-            const int levelID = m_level->m_levelID;
-            const int tier = RatingsManager::getDemonTier(levelID);
-            if(tier != -1) {
-                updateButton(tier);
-                m_fields->gddlTierUpdated = true;
-            }
+            menu->setScale(0.5f);
+        }
+
+        const int levelID = m_level->m_levelID;
+
+        // just so it displays a bit earlier
+        const auto button = CCMenuItemSpriteExtra::create(
+            Utils::getSpriteFromTier(RatingsManager::getCachedTier(levelID)),
+            this, menu_selector(GDDLInfoLayer::onGDDLInfo
+        ));
+        button->setID("rating"_spr);
+        menu->addChildAtPosition(button, Anchor::Center);
+        m_fields->tierButton = button;
+
+        const int tier = RatingsManager::getDemonTier(levelID);
+
+        if (tier != -1) {
+            updateButton(tier);
+            m_fields->gddlTierUpdated = true;
         }
 
         return true;
@@ -143,43 +127,11 @@ class $modify(GDDLInfoLayer, LevelInfoLayer) {
         }
     }
 
-    void placeGDDLButton(const CCPoint& menuPosition, const CCSize& menuSize, const CCPoint& buttonPosition,
-                         const float buttonScale = 1.0f) {
-        const auto menu = CCMenu::create();
-        menu->setPosition(menuPosition);
-        menu->setContentSize(menuSize);
-        menu->setScale(buttonScale);
-        menu->setID("rating-menu"_spr);
-        addChild(menu);
-
-        const int levelID = m_level->m_levelID;
-        // just so it displays a bit earlier
-        const auto button = CCMenuItemSpriteExtra::create(Utils::getSpriteFromTier(RatingsManager::getCachedTier(levelID)), this, menu_selector(GDDLInfoLayer::onGDDLInfo));
-        button->setPosition(buttonPosition);
-        button->setID("rating"_spr);
-        menu->addChild(button);
-    }
-
     void updateButton(const int tier) {
-        const bool displayAsLabel = static_pointer_cast<UseOldTierLabelSettingV3>(Mod::get()->getSetting("use-old-tier-label"))->isEnabled();
-        if (!displayAsLabel) {
-            const auto menu = typeinfo_cast<CCMenu*>(getChildByID("rating-menu"_spr));
-            if (!menu)
-                return;
-            const auto tierButton = typeinfo_cast<CCMenuItemSpriteExtra*>(menu->getChildByID("rating"_spr));
-            if (!tierButton) return;
-            tierButton->removeAllChildren();
+        if (!m_fields->tierButton) return;
 
-            const auto tierSprite = Utils::getSpriteFromTier(tier);
-            tierButton->addChild(tierSprite);
-        } else {
-            const auto tierLabelSprite = typeinfo_cast<CCLabelBMFont*>(getChildByIDRecursive("gddl-rating_label"_spr));
-            if (!tierLabelSprite)
-                return;
-            const std::string newLabelContent = "Tier " + std::to_string(tier);
-            tierLabelSprite->setString(newLabelContent.c_str());
-            tierLabelSprite->setColor(RatingsManager::getTierColor(tier));
-        }
+        m_fields->tierButton->removeAllChildren();
+        m_fields->tierButton->addChild(Utils::getSpriteFromTier(tier));
     }
 
     // ReSharper disable once CppMemberFunctionMayBeConst
